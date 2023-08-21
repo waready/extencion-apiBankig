9ad887c0157aa52ec3b419cfc7769f07771f12cf
waready <elbarto119@hotmail.com>
2023-08-21 10:04:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Markdown Code Formatter Extension
+
+![Extension Icon](https://example.com/extension-icon.png)
+
+This extension adds the `<code-group>` and `<code-block>` tags to code blocks containing XML or JSON in Markdown files.
+
+## Installation
+
+You can install the extension from the [Visual Studio Code Marketplace](https://marketplace.visualstudio.com/) or by following these steps:
+
+1. Open Visual Studio Code.
+2. Go to the Extensions view by clicking on the square icon in the sidebar on the left or pressing `Ctrl+Shift+X`.
+3. Search for "Markdown Code Formatter" and click on the "Install" button.
+
+## Usage
+
+1. Open a Markdown file that contains code blocks.
+2. Use the following format to create code blocks for XML or JSON:
+
+   ```xml
+   <!-- Your XML code here -->
+   ```
+
+   ```json
+   // Your JSON code here
+   ```
+
+3. To format the code blocks, use the command palette (`Ctrl+Shift+P`), search for "Format Code Blocks," and select it.
+
+## Examples
+
+### XML Code Block
+
+<code-group>
+<code-block title="XML" active>
+
+```xml
+<!-- Your XML code here -->
+```
---
+++ src/extension.ts
@@ -0,0 +1,132 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+// async function formatCodeBlocks() {
+//     const editor = vscode.window.activeTextEditor;
+
+//     if (editor) {
+//         const document = editor.document;
+//         const text = document.getText();
+
+//         // Patrón de bloque de código XML o JSON en Markdown
+//         const pattern = /```(xml|json)\n([\s\S]*?)\n```/g;
+
+//         const newText = text.replace(pattern, (_match, lang, code) => {
+//             return `<code-group>\n<code-block title="${lang.toUpperCase()}" active>\n\`\`\`${lang}\n${code}\n\`\`\`\n</code-block>\n</code-group>`;
+//         });
+
+//         if (newText !== text) {
+//             const edit = new vscode.WorkspaceEdit();
+//             edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newText);
+//             await vscode.workspace.applyEdit(edit);
+//         }
+//     }
+// }
+// async function formatCodeBlocks() {
+//     const editor = vscode.window.activeTextEditor;
+
+//     if (editor) {
+//         const document = editor.document;
+//         const text = document.getText();
+
+//         // Patrón de bloque de código XML o JSON en Markdown
+//         const pattern = /```(xml|json)\n([\s\S]*?)\n```/g;
+
+//         const newText = text.replace(pattern, (_match, lang, code) => {
+//             const isActive = lang === 'xml' ? ' active' : '';
+//             return `<code-group>\n<code-block title="${lang.toUpperCase()}"${isActive}>\n\`\`\`${lang}\n${code}\n\`\`\`\n</code-block>\n</code-group>`;
+//         });
+
+//         if (newText !== text) {
+//             const edit = new vscode.WorkspaceEdit();
+//             edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newText);
+//             await vscode.workspace.applyEdit(edit);
+//         }
+//     }
+//}
+
+// async function formatCodeBlocks() {
+//     const editor = vscode.window.activeTextEditor;
+
+//     if (editor) {
+//         const document = editor.document;
+//         const text = document.getText();
+
+//         // Patrón de bloque de código XML o JSON en Markdown
+//         const pattern = /```(xml|json)\n([\s\S]*?)\n```/g;
+
+//         let newText = text.replace(pattern, (_match, lang, code) => {
+//             const title = lang.toUpperCase();
+//             const isActive = lang === 'xml' ? ' active' : '';
+//             return `<code-group>\n<code-block title="${title}"${isActive}>\n\`\`\`${lang}\n${code}\n\`\`\`\n</code-block>`;
+//         });
+
+//         // Reemplazar el cierre de tags </code-group> con un único cierre
+//         newText = newText.replace(/<\/code-block>\n<\/code-group>/g, '</code-block>\n</code-group>');
+
+//         if (newText !== text) {
+//             const edit = new vscode.WorkspaceEdit();
+//             edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newText);
+//             await vscode.workspace.applyEdit(edit);
+//         }
+//     }
+// }
+
+
+
+async function formatCodeBlocks() {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+
+        // Patrón de bloque de código XML o JSON en Markdown
+        const pattern = /```(xml|json)\n([\s\S]*?)\n```/g;
+
+        let newText = text.replace(pattern, (_match, lang, code) => {
+            const title = lang.toUpperCase();
+            const isActive = lang === 'xml' ? ' active' : '';
+            return `<code-group>\n<code-block title="${title}"${isActive}>\n\`\`\`${lang}\n${code}\n\`\`\`\n</code-block>\n</code-group>`;
+        });
+
+         // Reemplazar secuencia "</code-group>\n<code-group>" por un espacio
+        newText = newText.replace(/<\/code-group>\n<code-group>/g, ' ');
+
+        // Reemplazar cierre de tag </code-group> seguido de apertura <code-group> por un espacio
+        newText = newText.replace(/<\/code-group>\s*<code-group>/g, ' ');
+         
+        if (newText !== text) {
+            const edit = new vscode.WorkspaceEdit();
+            edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newText);
+            await vscode.workspace.applyEdit(edit);
+        }
+    }
+}
+
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "apibanking" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('apibanking.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from ApiBanking!');
+		formatCodeBlocks();
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
